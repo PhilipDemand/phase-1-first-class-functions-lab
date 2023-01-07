1a84e4df75d613fa8a911ff2e7a62ab94ed566ed
Philip Demand <philipdemand@yahoo.com>
2023-01-07 14:35:47 -0500

    commit tree 71133d9ee107482f55b554a0db3e8e8f27658245

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (array) {
+    let newArray = array.slice(0, 2)
+    return newArray;
+ };
+
+ const returnLastTwoDrivers = function (array) {
+    let newArray2 = array.slice(Math.max(array.length - 2, 0))
+    return newArray2;
+ }
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+ function createFareMultiplier(multiplier) {
+    return function (fare) {
+      let output = fare * multiplier;
+      return output;
+    }
+  }
+
+  const fareDoubler = createFareMultiplier(2);
+
+  const fareTripler = createFareMultiplier(3);
+
+  function selectDifferentDrivers(array, firstOrLastFunc) {
+    if (firstOrLastFunc === returnFirstTwoDrivers) {
+      return array.slice(0,2)
+    } else if (firstOrLastFunc === returnLastTwoDrivers) {
+      return array.slice(Math.max(array.length - 2, 0))
+    }
+  }
